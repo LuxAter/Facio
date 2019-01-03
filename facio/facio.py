006d1e0353dfd6cbcde7e866ce74bc96919551d9
Arden Rasmussen <ardenisthebest@gmail.com>
2019-01-02 16:17:54 -0800

    Added more commands

--- facio/facio.py
+++ facio/facio.py
@@ -4,7 +4,7 @@ Makefile geneorator for C/C++ projects.
 """
 import json
 import os
-import sys
+import argparse
 
 
 def end_replace(string, terms):
@@ -27,35 +27,6 @@ def recurse_replace(obj, terms):
         elif isinstance(obj[key], str):
             obj[key] = end_replace(obj[key], terms)
     return obj
-
-def load_conf():
-    """Reads facio.conf file if no config.json file is found"""
-
-def load_config():
-    """Reads config.json file"""
-    raw = {}
-    with open("config.json", 'r') as file:
-        raw = json.load(file)
-    settings = {'root': os.getcwd()}
-    targets = {}
-    for key, value in raw.items():
-        if isinstance(value, str):
-            settings[key] = value
-        elif isinstance(value, list):
-            settings[key] = ' '.join(value)
-        else:
-            targets[key] = value
-    for key in settings:
-        for key_sub, value_sub in settings.items():
-            settings[key] = settings[key].replace("{{" + key_sub + "}}",
-                                                  value_sub)
-    for key in targets:
-        targets[key] = recurse_replace(targets[key], settings)
-        if 'name' not in targets[key]:
-            targets[key]['name'] = key
-        targets[key]['title'] = targets[key]['name'].upper()
-    return settings, targets
-
 
 def save_makefile(makefile):
     """Saves makefile"""
@@ -124,7 +95,6 @@ def gen_lib(target, targets):
         target['clean_deps'] = "clean-" + " clean-".join(target['dep'])
         target['link_deps'] = [x for x in target['dep'] if targets[x]['type'] in ('lib', 'aumake', 'make', 'faumake')]
         target['dep'] = "build-" + " build-".join(target['dep'])
-        print(target['link_deps'])
     else:
         target['dep'] = ''
         target['clean_deps'] = ''
@@ -142,8 +112,6 @@ def gen_lib(target, targets):
             target['files_exclude'], target['file_find'])
     if 'join' in target and target['join'] is True:
         target['join'] = '\n\t'.join(["mkdir -p $(ROOT)/tmp/{0} && cd $(ROOT)/tmp/{0} && ar x {1}/lib/{0} && ar qc $(ROOT)/$@ $(ROOT)/tmp/{0}/*.o && rm -rf $(ROOT)/tmp/{0}".format(x, targets[x]['path']) for x in target['link_deps']])
-        print("JOINING!")
-        print(target['join'])
     else:
         target['join'] = ''
     return """
@@ -261,7 +229,6 @@ def gen_target(target, targets):
 
 
 def gen_pre(settings):
-    print(settings)
     return """
 SHELL=/bin/bash
 ifndef .VERBOSE
@@ -317,7 +284,6 @@ define help
 printf "%b%*s%b: %s\\n" "$(HELP_COLOR)" 20 "$(1)" "\\033[0m" "$(2)"
 endef
 
-
 """.format("\n".join(
         ["{}={}".format(key.upper(), value) for key, value in settings.items()]))
 
@@ -337,15 +303,163 @@ $(ROOT)/$(BUILD)/%.c.o: %.c
 FORCE:
 """
 
+def gen_group(targets):
+    def gen_cmd(name, pre, targets):
+            return "{}: {}\n".format(name, ' '.join("{}-{}".format(pre, x) for x in targets))
+    groups = {}
+    for key, val in targets.items():
+        if 'group' in val:
+            if val['group'] in groups:
+                groups[val['group']].append(key)
+            else:
+                groups[val['group']] = [key]
+    string = ""
+    if 'main' in groups:
+        string += gen_cmd("all", "build", groups['main'])
+        string += gen_cmd("clean", "clean", groups['main'])
+        string += gen_cmd("install", "install", groups['main'])
+        string += gen_cmd("uninstall", "uninstall", groups['main'])
+        string += '\n'
+    for key, val in groups.items():
+        if key == "main":
+            continue
+        string += gen_cmd("{}".format(key), "build", val)
+        string += gen_cmd("clean-{}".format(key), "clean", val)
+        string += gen_cmd("install-{}".format(key), "install", val)
+        string += gen_cmd("uninstall-{}".format(key), "uninstall", val)
+        string += '\n'
+    return string
+
+
+
+def parse_config(raw):
+    settings = {'root': os.getcwd()}
+    targets = {}
+    for key, value in raw.items():
+        if isinstance(value, str):
+            settings[key] = value
+        elif isinstance(value, list):
+            settings[key] = ' '.join(value)
+        else:
+            targets[key] = value
+    for key in settings:
+        for key_sub, value_sub in settings.items():
+            settings[key] = settings[key].replace("{{" + key_sub + "}}",
+                                                  value_sub)
+    for key in targets:
+        targets[key] = recurse_replace(targets[key], settings)
+        if 'name' not in targets[key]:
+            targets[key]['name'] = key
+        targets[key]['title'] = targets[key]['name'].upper()
+    return settings, targets
+
+def load_config():
+    """Loads config file"""
+    config = {}
+    if os.path.isfile("config.json"):
+        with open("config.json", 'r') as file:
+            config = json.load(file)
+    else:
+        print("ERROR: No config.json or facio.cfg file found")
+    return config
+
+def gen_config():
+    """Generates a default config.json file"""
+    config_base = {"CXX": "clang++", "CC": "clang", "CCIGNORE": "", "CXXIGNORE": "", "CCFLAGS": ["-fPIC -Wall -Wpedantic --static"], "CXXFLAGS": ["-std=c++17", "-fPIC", "-Wall", "-Wpedantic", "--static"], "LINK": "", "source": "src", "include_dir": "include", "include": [], "build":"build"}
+    with open("config.json", 'w') as file:
+        json.dump(config_base, file)
+
+def modify_config(config, args):
+    """Modifies both config.json if it exist"""
+    obj = {'path': args.path.replace('[[', '$(').replace(']]', ')')}
+    if 'exclude' in args and args.exclude and args.exclude not in ('none', 'None', ''):
+        obj['files_exclude'] = args.exclude.replace('[[', '$(').replace(']]', ')')
+    if 'files' in args and args.files:
+        obj['files'] = args.files.replace('[[', '$(').replace(']]', ')')
+    if 'deps' in args and args.deps:
+        obj['deps'] = args.deps
+    if 'join' in args and args.join:
+        obj['join'] = True
+    if 'source' in args and args.source:
+        obj['source'] = args.source.replace('[[', '$(').replace(']]', ')')
+    if 'group' in args and args.group and args.group not in ('none', 'None', ''):
+        obj['group'] = args.group
+    if args.mtype == 'config':
+        config[args.name] = args.value.replace('[[', '$(').replace(']]', ')')
+    elif args.mtype == 'exe':
+        obj['type'] = 'exe'
+        config[args.name] = obj
+    elif args.mtype == 'lib':
+        obj['type'] = 'lib'
+        config[args.name] = obj
+    elif args.mtype == 'aumake':
+        obj['type'] = 'aumake'
+        config[args.name] = obj
+    elif args.mtype == 'faumake':
+        obj['type'] = 'faumake'
+        config[args.name] = obj
+    elif args.mtype == 'make':
+        obj['type'] = 'make'
+        config[args.name] = obj
+    with open("config.json", 'w') as file:
+        json.dump(config, file)
 
 def main():
     """Core component of the facio genorator"""
-    settings, targets = load_config()
-    makefile = gen_pre(settings)
-    for key in targets:
-        makefile += gen_target(targets[key], targets)
-    makefile += gen_post(settings)
-    save_makefile(makefile)
+    parser = argparse.ArgumentParser("Facio")
+    subparsers = parser.add_subparsers(dest='command')
+    modify = subparsers.add_parser('add', help="modifies current config file")
+    parser.add_argument("--gen-config", action='store_true', help="Generates config from either base config or from facio.conf")
+    parser.add_argument('config', nargs='?', type=str, default="", help="Config files to generate make file from")
+    msubparsers = modify.add_subparsers(dest='mtype')
+    config = msubparsers.add_parser('config', help="Add config variable")
+    config.add_argument('name', help="Name of the target")
+    config.add_argument('value', help="Value of the config variable")
+    exe = msubparsers.add_parser('exe', help="Add exe target")
+    exe.add_argument('name', help="Name of the target")
+    exe.add_argument('path', help="Destination path")
+    exe.add_argument('files', help="Files to use for target")
+    exe.add_argument('exclude', nargs="?", help="Files to exclude from target")
+    exe.add_argument('group', nargs='?', help="Adds target to group target")
+    exe.add_argument('deps', nargs='*', help="Target dependencies")
+    lib = msubparsers.add_parser('lib', help="Add lib target")
+    lib.add_argument('name', help="Name of the target")
+    lib.add_argument('path', help="Destination path")
+    lib.add_argument('files', help="Files to use for target")
+    lib.add_argument('exclude', nargs="?", help="Files to exclude from target")
+    lib.add_argument('group', nargs='?', help="Adds target to group target")
+    lib.add_argument('deps', nargs='*', help="Target dependencies")
+    lib.add_argument('--join', action="store_true", help="Joins target dependencies into one library")
+    aumake = msubparsers.add_parser('aumake', help="Add aumake target")
+    aumake.add_argument('name', help="Name of the target")
+    aumake.add_argument('path', help="Destination path")
+    aumake.add_argument('source', help="Source path")
+    aumake.add_argument('group', nargs='?', help="Adds target to group target")
+    faumake = msubparsers.add_parser('faumake', help="Add faumake target")
+    faumake.add_argument('name', help="Name of the target")
+    faumake.add_argument('path', help="Destination path")
+    faumake.add_argument('source', help="Source path")
+    faumake.add_argument('group', nargs='?', help="Adds target to group target")
+    make = msubparsers.add_parser('make', help="Add make target")
+    make.add_argument('name', help="Name of the target")
+    make.add_argument('path', help="Destination path")
+    make.add_argument('source', help="Source path")
+    make.add_argument('group', nargs='?', help="Adds target to group target")
+    args = parser.parse_args()
+    print(args)
+    config = load_config()
+    if args.command == 'add':
+        modify_config(config, args)
+    elif args.gen_config:
+        gen_config()
+    else:
+        settings, targets = parse_config(config)
+        makefile = gen_pre(settings)
+        makefile += gen_group(targets)
+        for key in targets:
+            makefile += gen_target(targets[key], targets)
+        makefile += gen_post(settings)
+        save_makefile(makefile)
 
 
 if __name__ == "__main__":
